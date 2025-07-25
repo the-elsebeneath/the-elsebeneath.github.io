f505409b3df9102e24719bfae738cec62b0902c0
Stephen Collins <sgcollins@outlook.com>
2025-07-24 20:46:57 -0400

    fixed hard coded URLs

--- astro.config.mjs
+++ astro.config.mjs
@@ -12,7 +12,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://the-elsebeneath.github.io",
+  site: "https://elsebeneath.online",
   base: "/",
   trailingSlash: "ignore",
   prefetch: {
